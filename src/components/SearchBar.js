8f8d7c89c5dbf5af1c50f40d03984f95f33c34a1
icestorm8 <shaked5001@gmail.com>
2024-06-06 17:10:17 +0300

    changed search bar to navigate to search results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar() {
+  const navigate = useNavigate();
+  const searchRef = useRef(null);
+
+  const handleSearch = () => {
+    navigate(`/?search=${searchRef.current.value}`);
+  };
   return (
     <div className="input-group w-25 container">
-      <input type="text" placeholder="search by..." className="form-control" />
-      <button className="btn btn-primary">Search</button>
+      <input
+        ref={searchRef}
+        type="text"
+        placeholder="search by..."
+        className="form-control"
+        onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+      />
+      <button className="btn btn-primary" onClick={handleSearch}>
+        Search
+      </button>
     </div>
   );
 }
